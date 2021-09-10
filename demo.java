da91c7b4379e7d5c2f657ddaf62053378a0cee4a
omkar1818 <90453293+omkar1818@users.noreply.github.com>
2021-09-10 16:36:38 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,11 @@
+class demo
+{
+public static void main ( String [] args)
+{
+System.out.println("JAVA");
+System.out.println(50);
+System.out.println(12.36);
+System.out.println(true);
+System.out.println('A');
+}
+}
